bd8ef57de56d99ce5c9a8ada7c9cd45193f128d5
Rocío Rodríguez <alu0100592832@ull.edu.es>
2015-05-25 05:33:35 +0100

    Modificando puerto

--- gruntfile.js
+++ gruntfile.js
@@ -96,7 +96,7 @@ module.exports = function(grunt) {
 			custom: {
 				options: {
 					'web-port': 1337,
-					'web-host': 'localhost',
+					'web-host': 'https://e-dash.herokuapp.com/',
 					'debug-port': 5858,
 					'save-live-edit': true,
 					'no-preload': true,
