f7db9629bf4037f66a4c2247514dc064d21be9b7
Rocío Rodríguez <alu0100592832@ull.edu.es>
2015-05-25 03:35:03 +0100

    Actualizando Procfile

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var init = require('./config/init')(),
  * Please note that the order of loading is important.
  */
 
- require('mongoose-middleware').initialize(mongoose);
+require('mongoose-middleware').initialize(mongoose);
 
 // Bootstrap db connection
 var db = mongoose.connect(config.db, function(err) {
